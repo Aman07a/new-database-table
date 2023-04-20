f532c02fb669d5bdd243eb48df92aef4722014f4
Aman07a <aman16.aa17@gmail.com>
2023-04-20 10:26:57 +0200

    Querying Our Table

--- inc/template-pets.php
+++ inc/template-pets.php
@@ -3,7 +3,8 @@
 get_header(); ?>
 
 <div class="page-banner">
-  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+  <div class="page-banner__bg-image" style="background-image: url(
+    <?php echo get_theme_file_uri("/images/ocean.jpg"); ?>);"></div>
   <div class="page-banner__content container container--narrow">
     <h1 class="page-banner__title">Pet Adoption</h1>
     <div class="page-banner__intro">
@@ -21,6 +22,13 @@ get_header(); ?>
     results (showing the first x).
   </p>
 
+  <?php
+  global $wpdb;
+  $tablename = $wpdb->prefix . "pets";
+  $ourQuery = $wpdb->prepare("SELECT * FROM $tablename LIMIT 100");
+  $pets = $wpdb->get_results($ourQuery);
+  ?>
+
   <table class="pet-adoption-table">
     <tr>
       <th>Name</th>
@@ -31,15 +39,17 @@ get_header(); ?>
       <th>Favorite Color</th>
       <th>Favorite Food</th>
     </tr>
-    <tr>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-      <td>-</td>
-    </tr>
+    <?php foreach ($pets as $pet) { ?>
+      <tr>
+        <td><?php echo $pet->petname; ?></td>
+        <td><?php echo $pet->species; ?></td>
+        <td><?php echo $pet->petweight; ?></td>
+        <td><?php echo $pet->birthyear; ?></td>
+        <td><?php echo $pet->favhobby; ?></td>
+        <td><?php echo $pet->favcolor; ?></td>
+        <td><?php echo $pet->favfood; ?></td>
+      </tr>
+    <?php } ?>
   </table>
 </div>
 
